957a9bc3b038db571ed7fad38585342dce8cd04f
24374ap <24374ap@hvhs.school.nz>
2026-06-19 03:04:01 +0000

    Jun 19, 2026, 3:04 AM

--- script.js
+++ script.js
@@ -15,4 +15,3 @@ alert("weicome to my pogram");
 uesName=prompt('whts is your name?');
 alert(" Hi " + uesName + " welcome to turkish shop");
 prompt('plaes enter your age');
-prompt('plaes enter your age',14);
