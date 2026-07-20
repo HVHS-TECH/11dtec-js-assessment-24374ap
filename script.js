9ec3be0e7f6ac8191221e9f043af883529f93000
24374ap <24374ap@hvhs.school.nz>
2026-07-20 21:47:06 +0000

    Jul 20, 2026, 9:47 PM

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ alert("Welcome to my program");
 uesName=prompt('What is your name?');
 alert(" Hi " + uesName + " welcome to turkish shop");
 uesage=prompt('plaes enter your age');
+console.log(uesage)
 prompt('ok and password');
-alert("this is you turkish menu card ")
-alert("pick one your favorite food")
+alert("this is you turkish menu card ");
+alert("pick one your favorite food");
